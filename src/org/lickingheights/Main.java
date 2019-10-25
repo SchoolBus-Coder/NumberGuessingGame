17fb007d47d7bb2e75cbd0b7ef64c177108b90df
Zac Miller <zahary.miller.2023@student.lhschools.org>
2019-10-25 09:28:45 -0400

    Full Java Commit

---
+++ src/org/lickingheights/Main.java
@@ -0,0 +1,13 @@
+package org.lickingheights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    Scanner keyboard;
+    Scanner keyboard = new Scanner (System.in);
+
+
+    }
+}
